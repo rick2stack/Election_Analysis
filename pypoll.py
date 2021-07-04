bd55900df562627f9377ed8267c1264ff53a1002
rick2stack <82985823+rick2stack@users.noreply.github.com>
2021-07-04 11:03:02 -0500

    updated py code

--- pypoll.py
+++ pypoll.py
@@ -1,21 +1,65 @@
 # Add our dependencies.
 import csv
 import os
+# Add our dependencies.
+import csv
+import os
 # Assign a variable to load a file from a path.
 file_to_load = os.path.join("Resources", "election_results.csv")
 # Assign a variable to save the file to a path.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
-# Open the election results and read the file.
-with open(file_to_load) as election_data:
+# 1. declaring dicitionaries.
+total_votes = 0
+candidate_options=[]
+candidate_votes={}
+#declaring winning cadidate and tracker
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
 
-    # To do: read and analyze the data here.
-        # Read the file object with the reader function.
+
+# Open the election results and read the file
+with open(file_to_load) as election_data:
     file_reader = csv.reader(election_data)
 
-    # Print each row in the CSV file.
+    # Read the header row.
     headers = next(file_reader)
-    print(headers)
+
+    # Print each row in the CSV file.
+    for row in file_reader:
+        # 2. Add to the total vote count. the += is augemented version of a=a+1
+        total_votes +=  1
+        # Declare that the candidate is in column 3, aka index 2.
+        candidate_name = row[2]
+
+        # If the candidate does not match any existing candidate...
+        if candidate_name not in candidate_options:
+            # Add it to the list of candidates.
+            candidate_options.append(candidate_name)
+            # this will add a key to the dict
+            candidate_votes[candidate_name] = 0
+        # Add a vote to that candidate's count.
+        candidate_votes[candidate_name] += 1
+    # Determine the percentage of votes for each candidate by looping through the counts.
+# 1. Iterate through the candidate list. candidate_votes is a dic so the fo loop will automatically assume the key in the dic
+    for candidate_name in candidate_votes:
+        # 2. Retrieve vote count of a candidate. the [x] is the key
+        votes = candidate_votes[candidate_name]
+        # 3. Calculate the percentage of votes.
+        vote_percentage = float(votes) / float(total_votes) * 100
+        # 4. Print the candidate name and percentage of votes.
+        print(f"{candidate_name}: received {vote_percentage}% of the vote.")
+        if (votes > winning_count) and (vote_percentage > winning_percentage):
+            # If true then set winning_count = votes and winning_percent =
+            # vote_percentage.
+            winning_count = votes
+            winning_percentage = vote_percentage
+            # And, set the winning_candidate equal to the candidate's name.
+            winning_candidate = candidate_name
+    print(f"The winning candidate is {winning_candidate} with {winning_percentage:.1f}%, and a total of {votes:,} votes)\n")
+
+
 
 
 #Write down the names of all the candidates.
